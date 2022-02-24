eafc9393c087e9e30c11ec8495008b34ad221f41
guptarajiv535 <guptarajiv535@gmail.com>
2022-02-25 01:27:24 +0530

    [Day1] Functions and Variables assigment.

--- src/main/kotlin/com/codingpanda/maven/kotlin/Hello.kt
+++ src/main/kotlin/com/codingpanda/maven/kotlin/Hello.kt
@@ -2,5 +2,20 @@ package com.codingpanda.maven.kotlin
 
 fun main(args: Array<String>) {
     println("Hello, World")
+    val a: Int =5 // immediate assignment
+    val b =9 // `Int` type is inferred
+    val c = 14
+    val d : Int // Type required when no initializer is provided
+    d = 3 // deferred assignment
+    println("Sum of $a and $b is: ${sum(a,b)}")
+    println("Subtraction of $c and $d is: ${subtract(c,d)}")
 }
+
+/*A function with two Int parameters and Int return type.*/
+fun sum(a: Int,b: Int): Int{
+   return a+b
+}
+
+/*A function body can be an expression. Its return type is understood implicitly.*/
+fun subtract(c: Int,d:Int) = c-d
 
